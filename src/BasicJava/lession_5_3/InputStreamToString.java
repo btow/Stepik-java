b5b3cf05e6862f36328bafcd6dae4c6e2c3c95db
btow <btow@yandex.ru>
2016-11-14 13:37:31 +0500

    The lession 5.3

--- src/BasicJava/lession_5_3/InputStreamToString.java
+++ src/BasicJava/lession_5_3/InputStreamToString.java
@@ -3,6 +3,7 @@ package BasicJava.lession_5_3;
 import java.io.*;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 
 /**
  * The program reads data from an InputStream and converts
@@ -13,7 +14,7 @@ public class InputStreamToString {
 
     public static void main(String[] args) {
         Charset charset = StandardCharsets.US_ASCII;
-        try (InputStream inputStream = System.in) {
+        try (InputStream inputStream = new ByteArrayInputStream(new byte[] {48, 49, 50, 51, 13, 10})) {
             String textForOut = readAsString(inputStream, charset);
             System.out.println(textForOut);
         } catch (IOException e) {
@@ -21,37 +22,25 @@ public class InputStreamToString {
         }
     }
 
-    public static String readAsString(InputStream inputStream, Charset charset) throws IOException {
+    private static String readAsString(InputStream inputStream, Charset charset) throws IOException {
         // your implementation here
-        int resultsRead = -1, beginIndex = 0;
-        byte[] bytes = new byte[1024], bytes1 = null;
+        int resultsRead, deltaStepOfArray = 512, beginIndex = 0, endIndex = deltaStepOfArray;
+        byte[] bytes = new byte[endIndex], bytes1;
         do {
-            resultsRead = inputStream.read(bytes);
+            resultsRead = inputStream.read();
             if (resultsRead > -1) {
-                bytes1 = new byte[resultsRead + beginIndex];
-                for (int i = beginIndex; i < (beginIndex + resultsRead); i++) {
-                    bytes1[i] = bytes[i - beginIndex];
+                bytes[beginIndex] = (byte) resultsRead;
+                beginIndex++;
+                if (beginIndex == endIndex) {
+                    endIndex += deltaStepOfArray;
+                    bytes1 = Arrays.copyOf(bytes,endIndex);
+                    bytes = bytes1;
                 }
-                beginIndex += resultsRead;
             }
         } while (resultsRead > -1);
 
-//        byte[] bytes1 = {48, 49, 50, 51};
+        bytes1 = Arrays.copyOf(bytes, beginIndex);
 
-        char[] chars = new char[bytes1.length];
-        String textForOut = "";
-        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(bytes1);
-        try (Reader reader = new InputStreamReader(byteArrayInputStream, charset)) {
-            resultsRead = reader.read(chars);
-            if (resultsRead != -1) {
-                for (char character : chars) {
-                    textForOut += ((Character) character).toString();
-                }
-            }
-        } catch (IOException e) {
-            throw e;
-        }
-
-        return textForOut;
+        return new String(bytes1, charset);
     }
 }
